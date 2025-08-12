e6d26f2500c55ba3ad1dc0034bde723ec4342b4c
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2025-08-12 08:55:20 -0300

    fix: tintColor code

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,7 +11,7 @@ function TabIcon({ focused, icon, title }: any) {
         source={images.highlight}
         className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
       >
-        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Image source={icon} tintColor={"#151312"} className="size-5" />
         <Text className="text-secondary text-base font-semibold ml-2">
           {title}
         </Text>
@@ -21,7 +21,7 @@ function TabIcon({ focused, icon, title }: any) {
 
   return (
     <View className="size-full justify-center items-center mt-4 rounded-full">
-      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+      <Image source={icon} tintColor={"#A8B5DB"} className="size-5" />
     </View>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,21 @@
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import useFetch from "@/hooks/useFetch";
+import { fetchMovies } from "@/services/api";
+import { Text } from "@react-navigation/elements";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, Image, ScrollView, View } from "react-native";
 import "../global.css";
 
 export default function Index() {
   const router = useRouter();
+
+    const {
+      data: movies,
+      loading: moviesLoading,
+      error: moviesError
+    } = useFetch(()=> fetchMovies({ query: "" }))
 
   return (
     <View className="flex-1 bg-primary">
@@ -20,12 +29,27 @@ export default function Index() {
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
 
-        <View className="flex-1 mt-5">
+        {moviesLoading ? (
+          <ActivityIndicator
+          size={"large"}
+          color={"#0000ff"}
+          className="mt-10 self-center"
+          />
+        ) : moviesError ? (
+          <Text>Error: {moviesError?.message}</Text>
+        ): (
+          <View className="flex-1 mt-5">
           <SearchBar 
           onPress={()=> router.push("/search")}
           placeholder="Search for a movie"
           />
         </View>
+        )}
+
+        <>
+          <Text className="text-lg text-white font-bold mt-5 mb-3">Latest Movies</Text>
+        </>
+
       </ScrollView>
     </View>
   );
